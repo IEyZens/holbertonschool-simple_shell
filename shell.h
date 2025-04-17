807fa85f3a411606ad830a5f080f4e584e72f405
IEyZens <roncint61@gmail.com>
2025-04-17 09:35:28 +0200

    Add function and library

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+int main(void);
+
+#endif /* SHELL_H */
