8b0ab463a0fc500ed7e5717159d054ee0a3af47b
IEyZens <roncint61@gmail.com>
2025-04-18 14:20:54 +0200

    Update fonction executor (void > int)

--- shell.h
+++ shell.h
@@ -10,7 +10,7 @@
 extern char **environ;
 
 char *display_prompt(void);
-void execute_command(char *input);
+int execute_command(char *input);
 void print_error(void);
 void free_and_null(char **ptr);
 int _strlen(char *str);
