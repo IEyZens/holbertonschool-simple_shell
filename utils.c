63077fc81fb1faf6be0d2af55be6aa1a3875a88e
IEyZens <roncint61@gmail.com>
2025-04-18 17:22:50 +0200

    Add file utils (new)

--- utils.c
+++ utils.c
@@ -1,16 +1,8 @@
 #include "shell.h"
 
 /**
- * print_error - Il permet de print les erreurs pour les fichiers .c
- */
-void print_error(void)
-{
-	write(2, "./shell: No such file or directory\n", 36);
-}
-
-/**
- * free_and_null - Il permet de free dans les fichiers .c
- * @ptr: est un char double pointeur
+ * free_and_null - Libère un pointeur et le met à NULL
+ * @ptr: double pointeur à libérer
  */
 void free_and_null(char **ptr)
 {
