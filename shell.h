a4fcbf0a250db5289dfefaade0b7f948220c9e36
IEyZens <roncint61@gmail.com>
2025-04-18 14:34:56 +0200

    Return at the void for function executor

--- shell.h
+++ shell.h
@@ -10,7 +10,7 @@
 extern char **environ;
 
 char *display_prompt(void);
-int execute_command(char *input);
+void execute_command(char *input);
 void print_error(void);
 void free_and_null(char **ptr);
 int _strlen(char *str);
