f168b94833c097ea2962d921d73eec69f9df3dd6
IEyZens <roncint61@gmail.com>
2025-04-20 20:22:32 +0200

    Add function for task 4 (Simple Shell 0.3)

--- shell.h
+++ shell.h
@@ -14,5 +14,16 @@ char *read_command(void);
 void execute_command(char *command);
 char **parse_args(char *command);
 void print_error(void);
+int _strncmp(const char *s1, const char *s2, int n);
+char *_strdup(const char *str);
+int _strlen(const char *s);
+void _strcat(char *dest, const char *src);
+char *_strcpy(char *dest, const char *src);
+char *get_path_env(void);
+char *build_full_path(char *dir, char *command);
+char *find_command_path(char *command);
+void run_child_process(char *path, char **argv);
+void clean_and_return(char *path, char **argv, char *original);
+char *resolve_path(char *command);
 
 #endif /* SHELL_H */
