4d2d53c6d0880f9a984e6492f990dd37cde8a010
IEyZens <roncint61@gmail.com>
2025-04-20 17:38:48 +0200

    Add function parse_args

--- shell.h
+++ shell.h
@@ -6,11 +6,13 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/signal.h>
+#include <string.h>
 
 extern char **environ;
 
 char *read_command(void);
 void execute_command(char *command);
+char **parse_args(char *command);
 void print_error(void);
 
 #endif /* SHELL_H */
