a6e52949dec62572697e6b611231d8b4078ecb3d
IEyZens <roncint61@gmail.com>
2025-04-20 22:38:31 +0200

    Add commentary for betty

--- builtins.c
+++ builtins.c
@@ -1,24 +1,28 @@
 #include "shell.h"
 
 /**
+ * handle_builtin - Permet d'exécuter les commandes exit et env
+ * @command: est un char pointeur
  *
+ * Return: Always 0
  */
-#include "shell.h"
-
 int handle_builtin(char *command)
 {
 	if (_strcmp(command, "exit") == 0)
-	{
 		return (1);
-	}
-	else if (_strcmp(command, "env") == 0)
+
+	if (_strcmp(command, "env") == 0)
 	{
 		print_env();
 		return (0);
 	}
+
 	return (-1);
 }
 
+/**
+ * print_env - Permet d'imprimer l'environnement actuel
+ */
 void print_env(void)
 {
 	int i;
