ca5191dcfea0cc164f19651f4bbc633862e438d8
IEyZens <roncint61@gmail.com>
2025-04-20 20:22:16 +0200

    Add function for function run_child_process and clean_and_return

--- utils.c
+++ utils.c
@@ -7,3 +7,29 @@ void print_error(void)
 {
 	write(STDERR_FILENO, "./shell: No such file or directory\n", 35);
 }
+
+/**
+ * run_child_process - Exécuter la commande dans le processus enfant
+ * @path: est un char pointeur
+ * @argv: est un char double pointeur
+ */
+void run_child_process(char *path, char **argv)
+{
+	execve(path, argv, environ);
+	print_error();
+	exit(1);
+}
+
+/**
+ * clean_and_return - Libère la mémoire allouée (argv et PATH)
+ * à la fin de l'exécution ou en cas d'erreur
+ * @path: est un char pointeur
+ * @argv: est un char double pointeur
+ * @original: est un char pointeur
+ */
+void clean_and_return(char *path, char **argv, char *original)
+{
+	if (path != original)
+		free(path);
+	free(argv);
+}
