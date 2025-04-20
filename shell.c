305d3b00a3b06905d8a11dd96ff6522bf22bfc96
IEyZens <roncint61@gmail.com>
2025-04-20 17:05:12 +0200

    Add file for loop main

---
+++ shell.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/**
+ * main - Boucle principale du shell
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *command;
+
+	while (1)
+	{
+		command = read_command();
+
+		if (command == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
+
+		execute_command(command);
+		free(command);
+	}
+
+	return (0);
+}
